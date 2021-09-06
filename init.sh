1e428c07bd2b09c5be8d790c375aa5983468598e
Cameron Gregory <cameronrkgregory@protonmail.com>
2021-09-06 18:18:09 +0000

    feat: samba install in init.sh\n nginx and transmission tweaks

--- init.sh
+++ init.sh
@@ -24,6 +24,15 @@ sudo usermod -aG docker $USER
 
 #docker Compose
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+#Samba
+sudo mkdir /media
+sudo apt-get install samba
+sudo echo -e "[media]\n    comment = Media Folder\n    path = /media\n    read only = no\n    browsable = yes" >>  /etc/samba/smb.conf
+sudo service smbd restart
+sudo ufw allow samba
+sudo smbpasswd -a $USER
